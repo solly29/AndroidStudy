4e4b64557d55e7c18804aba375a70198f004940d
solly29 <44259984+solly29@users.noreply.github.com>
2020-05-13 16:27:18 +0900

    카카오 로그인 연동
    아직 좀더 공부해야된다
    카카오 문서보면서

--- kakaoLoginTest/app/src/main/java/com/example/kakaologintest/MainActivity.kt
+++ kakaoLoginTest/app/src/main/java/com/example/kakaologintest/MainActivity.kt
@@ -1,12 +1,88 @@
 package com.example.kakaologintest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.kakao.auth.AuthType
+import com.kakao.auth.Session
+import com.kakao.usermgmt.UserManagement
+import com.kakao.usermgmt.callback.LogoutResponseCallback
+import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val sessionCallback: SessionCallback = SessionCallback()
+    var session: Session? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        session = Session.getCurrentSession()
+        session?.addCallback(sessionCallback)
+
+        btn_custom_login.setOnClickListener{
+                session?.open(AuthType.KAKAO_LOGIN_ALL, this@MainActivity)
+        }
+
+        btn_custom_login_out.setOnClickListener{
+                UserManagement.getInstance()
+                    .requestLogout(object : LogoutResponseCallback() {
+                        override fun onCompleteLogout() {
+                            Toast.makeText(this@MainActivity, "로그아웃 되었습니다.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    })
+            }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // 세션 콜백 삭제
+        Session.getCurrentSession().removeCallback(sessionCallback)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
+        if (Session.getCurrentSession()
+                .handleActivityResult(requestCode, resultCode, data)
+        ) {
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    // 어플 해시키 가져오는 메소드
+    private fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
     }
 }
