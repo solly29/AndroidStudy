0e5532ff4a86c766da47242de3840ce3f781cc51
solly29 <cksdud1311@naver.com>
2021-10-15 23:32:54 +0900

    jetpack compose 튜토리얼

--- composeTest/app/src/main/java/com/example/composetest/MainActivity.kt
+++ composeTest/app/src/main/java/com/example/composetest/MainActivity.kt
@@ -1,25 +1,39 @@
 package com.example.composetest
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.renderscript.Sampler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            MessageCard(str = Message("test", "test"))
+            ComposeTestTheme {
+                Conversation(messages = SampleData.conversationSample)
+            }
+
+//            MessageCard(str = Message("test", "test"))
             /*ComposeTestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
@@ -41,15 +55,33 @@ fun MessageCard(str: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+//                .align(Alignment.CenterVertically)
         )
         
         Spacer(modifier = Modifier.width(8.dp))
-        
-        Column { // 세로로 나열할 수 있다.
 
-            Text(str.str1)
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+        
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) { // 세로로 나열할 수 있다.
+
+            Text(
+                text = str.str1,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(4.dp)) // 뷰와 같은 기능을함
-            Text(str.str2)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = str.str2,
+                    modifier = Modifier.padding(4.dp),
+                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 
@@ -63,6 +95,43 @@ fun showMessageCard() {
         str = Message("adroid", "test")
     )
 }
+
+
+// 다크 모드 설정 프리뷰
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    ComposeTestTheme {
+        MessageCard(
+            str = Message("Colleague", "Take a look at Jetpack Compose")
+        )
+    }
+}
+
+// 리스트 만들기
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) {
+            MessageCard(str = it)
+        }
+    }
+}
+
+// 리스트 프리뷰
+@Preview
+@Composable
+fun priviewConversation() {
+    ComposeTestTheme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
+
 
 /*
 @Composable
